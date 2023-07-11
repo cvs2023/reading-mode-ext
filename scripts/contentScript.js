67ba0a7c161330b7dc92a9dca925a6630993d2e6
cvs2023 <cvtheking464@gmail.com>
2023-07-11 22:23:33 +0530

    added .gitignore file

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -5,16 +5,20 @@ const article =
 if (article) {
   const text = article.textContent;
 
-  /*   // With Regular expression
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp); */
+  /*   
+  With Regular expression
+    const wordMatchRegExp = /[^\s]+/g;
+    const words = text.matchAll(wordMatchRegExp); */
 
   const words = text.split(" ");
 
-  // matchAll returns an iterator, convert to array to get word count
-  // const wordCount = [...words].length;
-  const wordCount = words.length;
+  /* 
+    Other way :
+      matchAll returns an iterator, convert to array to get word count
+      const wordCount = [...words].length;
+  */
 
+  const wordCount = words.length;
   const readingTime = Math.round(wordCount / 200);
 
   const badge = document.createElement("p");
@@ -22,10 +26,15 @@ if (article) {
   // Use the same styling as the publish information in an article's header
   badge.classList.add("color-secondary-text", "type--caption");
   badge.textContent = `⏱️ ${readingTime} min read @ReadingTime`;
+  badge.style.backgroundColor = "yellow";
+  badge.style.color = "green";
+  badge.style.padding = "10px";
+  badge.style.borderRadius = "10px";
 
   // Support for API reference docs
   const heading =
     article.querySelector("h1") || article.querySelector("header");
+
   // Support for article docs with date
   const date = article.querySelector("time")?.parentNode;
   (date ?? heading).insertAdjacentElement("afterend", badge);
